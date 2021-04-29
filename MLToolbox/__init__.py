d109069bb476dbc5e2a24b6294b6206e3cbea023
felix <zhangfelixx@hotmail.com>
2021-04-29 15:26:52 +0800

    Add the entry point of the module.

---
+++ MLToolbox/__init__.py
@@ -0,0 +1,4 @@
+"""MLToolbox
+
+This module is a machine learning toolbox for learners.
+"""
---
+++ MLToolbox/__main__.py
@@ -0,0 +1,14 @@
+# import sys
+# import os
+
+# if not __package__:
+#     path = os.path.join(os.path.dirname(__file__), os.pardir)
+#     sys.path.insert(0, path)
+
+
+def main():
+    print('You find the entry_point!')
+
+if __name__ == "__main__":
+    main()
+    a = [1,2,3]
