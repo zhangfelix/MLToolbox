111e93debad8c98e186d52c055ed9878b272e641
felix <zhangfelixx@hotmail.com>
2021-05-25 17:49:24 +0800

    Add knn's class framework

---
+++ MLToolbox/tools/data.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+def date_loader(filename):
+    # todo: load data from file.
+    pass
---
+++ MLToolbox/tools/distance.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+import numpy as np
+
+def euler_distance(a=np.array([]), b=np.array([])):
+    # todo: calculate euler distance
+    pass
