3c70d27cd9f3dd9885eb90c0c04e8721dbcaebba
felix <zhangfelixx@hotmail.com>
2021-09-30 19:06:44 +0800

    Update.

--- MLToolbox/navie_bayes.py
+++ MLToolbox/navie_bayes.py
@@ -9,13 +9,16 @@ class GaussianNB:
     """
     def __init__(self, var_smoothing=1e-09):
         self.var_smoothing = var_smoothing
-        self.class_priors = None
+        self.class_priors = []
         self.class_counts = None
         self.classes_map = { }
         self.means = None
         self.varances = None
+        self.train_nums = None
     def fit(self, X, y):
         """Fit model with datas."""
+        self.train_nums = len(y)
+        self.train_features = len(x[0])
         self.calculate_class_priors(y)
         self.calculate_means(X, y)
         self.calculate_varances(X,y)
@@ -26,10 +29,12 @@ class GaussianNB:
         self.class_counts = 0
         for lable, nums in class_counter.items():
             self.classes_map[lable] = self.class_counts
-            pass
+            self.class_counts += 1
+            self.class_priors.append(nums/self.train_nums)
 
-        pass
     def calculate_means(self, X, y):
-        pass
+        self.means = [[] for _ in range(self.train_nums)]
+        for idx in self.train_nums:
+            self.means[self.classes_map[y[idx]]]
     def calculate_varances(self, X, y):
         pass
