b2c3e80701c7f21ddfb69add86b123c55fd1591d
felix <zhangfelixx@hotmail.com>
2021-10-18 18:11:36 +0800

    Working on GaussianNB.

--- MLToolbox/navie_bayes.py
+++ MLToolbox/navie_bayes.py
@@ -4,17 +4,29 @@ Navie bayes algorithms.
 
 from collections import Counter
 class GaussianNB:
+    #todo(felix): 补全注释
     """
     GaussianNB Class.
+    Parameters
+    ----------
+    var_smoothing : float, default=1e-09
+        Gaussian smoothing parameter.
+    Attributes
+    ----------
+    class_priors : list of shape (n_classes)
+        Frequency of each category in the training set.
     """
     def __init__(self, var_smoothing=1e-09):
         self.var_smoothing = var_smoothing
         self.class_priors = []
         self.class_counts = None
+        # Map the class lables into indexes.
         self.classes_map = { }
         self.means = None
         self.varances = None
+        # Number of training items.
         self.train_nums = None
+        self.train_features = None
     def fit(self, X, y):
         """Fit model with datas."""
         self.train_nums = len(y)
@@ -25,11 +37,14 @@ class GaussianNB:
     def predict(self,new_points):
         pass
     def calculate_class_priors(self, y):
+        """Calculate the frequency of each category in the training set."""
         class_counter = Counter(y)
         self.class_counts = 0
         for lable, nums in class_counter.items():
+            # Map the class lables into indexes.
             self.classes_map[lable] = self.class_counts
             self.class_counts += 1
+            # Calculate the frequency of each category.
             self.class_priors.append(nums/self.train_nums)
 
     def calculate_means(self, X, y):
